b9c5592a9bfdf70ac6ec34b9d29c5802d8ae41dd
KacperW <kacek158@gmail.com>
2024-11-06 20:28:19 +0100

    Localization

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -23,5 +24,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/set-locale/{locale}', function ($locale) {
+    if (in_array($locale, ['en', 'pl'])) {
+        Session::put('locale', $locale);
+    }
+    return redirect()->back();
+})->name('set-locale');
 
 require __DIR__.'/auth.php';
