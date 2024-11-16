3ea4064d221335f3a72d7b6c0cdc5fedd9c229fc
KacperW <kacek158@gmail.com>
2024-11-16 23:00:51 +0100

    Database & Models

---
+++ database/seeders/PizzeriaSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Opening_hours;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PizzeriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('opening_hours')->insert([
+            [
+                'day' => 0,
+                'open_time' => "10:00:00",
+                'close_time' => "22:00:00"
+            ],
+            [
+                'day' => 1,
+                'open_time' => "10:00:00",
+                'close_time' => "22:00:00"
+            ],
+            [
+                'day' => 2,
+                'open_time' => "10:00:00",
+                'close_time' => "22:00:00"
+            ],
+            [
+                'day' => 3,
+                'open_time' => "10:00:00",
+                'close_time' => "22:00:00"
+            ],
+            [
+                'day' => 4,
+                'open_time' => "10:00:00",
+                'close_time' => "22:00:00"]
+            ,
+            [
+                'day' => 5,
+                'open_time' => "10:00:00",
+                'close_time' => "22:00:00"
+            ],
+            [
+                'day' => 6,
+                'open_time' => null,
+                'close_time' => null
+            ]
+
+        ]);
+    }
+}
