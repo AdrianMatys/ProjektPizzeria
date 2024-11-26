30801c15430e4b55063659513d4e9d1c53586c70
KacperW <kacek158@gmail.com>
2024-11-26 20:56:37 +0100

    Add PizzaSeeder to make seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             PizzeriaSeeder::class,
-            StorageSeeder::class
+            StorageSeeder::class,
+            PizzaSeeder::class
         ]);
     }
 }
