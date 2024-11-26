c7dc1aa56b21288a300ea31ebe91220eab94b3e6
KacperW <kacek158@gmail.com>
2024-11-26 01:18:22 +0100

    Seed default ingredients

---
+++ database/seeders/StorageSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StorageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('ingredients')->insert([
+            [
+                'name' => "Dough",
+                'quantity' => '5000',
+                'unit' => "g",
+            ],
+            [
+                'name' => "Tomato pizza sauce",
+                'quantity' => '3000',
+                'unit' => "g",
+            ],
+            [
+                'name' => "Ham",
+                'quantity' => '4000',
+                'unit' => "g",
+            ],
+            [
+                'name' => "Salami",
+                'quantity' => '5000',
+                'unit' => "g",
+            ],
+            [
+                'name' => "Oregano",
+                'quantity' => '800',
+                'unit' => "g",
+            ],
+        ]);
+        DB::table('translations')->insert([
+            [
+                'ingredient_id' => 1,
+                'language_code' => 'pl',
+                'name' => "Ciasto",
+            ],
+            [
+                'ingredient_id' => 2,
+                'language_code' => 'pl',
+                'name' => "Sos pomidorowy",
+            ],
+            [
+                'ingredient_id' => 5,
+                'language_code' => 'es',
+                'name' => "orégano",
+            ],
+        ]);
+    }
+}
