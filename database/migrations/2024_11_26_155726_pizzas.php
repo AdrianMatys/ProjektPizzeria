9a6925617fd12368fb74e1a8e7cfed594ab10c15
KacperW <kacek158@gmail.com>
2024-12-21 20:58:37 +0100

    CRUD pizza in menu

--- database/migrations/2024_11_26_155726_pizzas.php
+++ database/migrations/2024_11_26_155726_pizzas.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pizzas', function (Blueprint $table) {
         $table->id();
         $table->string('name');
+            $table->timestamps();
     });
         Schema::create('pizza_ingredients', function (Blueprint $table) {
             $table->id();
@@ -23,6 +24,8 @@ return new class extends Migration
             $table->foreignId('pizza_id')
                 ->constrained('pizzas')
                 ->onDelete('cascade');
+            $table->integer('quantity');
+            $table->timestamps();
         });
     }
 
---
+++ resources/views/management/employee/pizzas/edit.blade.php
@@ -0,0 +1,76 @@
+@include('shared.return-message')
+<button onclick="addNewIngredient()">Add new ingredient</button>
+<form action="{{ route('management.employee.pizzas.update', $pizza) }}" method="post">
+    @csrf
+    @method('put')
+    <br>
+    <label for="name">Name:</label><br>
+    <input type="text" name="name" id="name" value="{{$pizza->name}}"><br><br>
+
+    <table id="ingredientsTable">
+        <tr>
+            <th>ingredients</th>
+            <th>quantity (g)</th>
+            <th>remove</th>
+        </tr>
+        @foreach($pizza->ingredients as $ingredient)
+            <tr>
+                    <td>
+                        <select name="ingredient[]]" id="ingredient[]">
+
+                            @foreach($ingredients as $availableIngredient )
+                                @if($availableIngredient->name == $ingredient->name)
+                                    <option value="{{$availableIngredient->id}}" selected="selected">{{$availableIngredient->name}}</option>
+                                @else
+                                    <option value="{{$availableIngredient->id}}">{{$availableIngredient->name}}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </td>
+                    <td>
+                        <input type="number" name="quantity[]" id="quantity[]" value="{{ $ingredient->pivot->quantity}}">
+                    </td>
+                <td>
+                    <button type="button" onclick="removeIngredient(this)">X</button>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <button type="submit">Save pizza</button>
+</form>
+<style>
+    table, tr, td, th {
+        border: 1px solid black;
+        text-align: center;
+    }
+</style>
+
+<script>
+    let options = ''
+    @foreach($ingredients as $ingredient)
+        options += '<option value="{{$ingredient->id}}">{{$ingredient->name}}</option>'
+    @endforeach
+    function removeIngredient(button) {
+        let row = button.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+    }
+
+    function addNewIngredient() {
+        let table = document.getElementById('ingredientsTable')
+        let newRow = table.insertRow(-1);
+        let cell1 = newRow.insertCell(0);
+        let cell2 = newRow.insertCell(1);
+        let cell3 = newRow.insertCell(2);
+        cell1.innerHTML = ''+
+            '<select name="ingredient[]]" id="ingredient[]">' +
+            options +
+            '</select>'
+
+
+
+        cell2.innerHTML = '<input type="number" name="quantity[]" id="quantity[]">'
+        cell3.innerHTML = '<button onclick="removeIngredient(1)">X</button>'
+    }
+</script>
+
+
