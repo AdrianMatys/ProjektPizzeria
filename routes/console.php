c4a0ba4fbd3b402671f3111234a38bd06bb0e8ae
KacperW <kacek158@gmail.com>
2024-12-23 16:21:52 +0100

    Update import path

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Console\Commands\SendLowStockNotification;
+use App\Http\Controllers\Console\Commands\SendLowStockNotification;
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(SendLowStockNotification::class)->at('21:37');
+Schedule::command(SendLowStockNotification::class)->at('15:10');
