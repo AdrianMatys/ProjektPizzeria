aceb3945d3ded2856b98b8f6ac0e5d36475f70d6
KacperW <kacek158@gmail.com>
2024-12-24 22:46:56 +0100

    Add softdelete for Ingredient and Pizza

--- app/Models/Pizza.php
+++ app/Models/Pizza.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pizza extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = ['name'];
 
     public function ingredients(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
