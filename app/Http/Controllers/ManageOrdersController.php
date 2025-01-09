27be8c008806da645df3d441e3c8af150e75eed5
KacperW <kacek158@gmail.com>
2025-01-09 17:28:41 +0100

    Add groups in orders.index

--- app/Http/Controllers/ManageOrdersController.php
+++ app/Http/Controllers/ManageOrdersController.php
@@ -8,9 +8,10 @@ use Illuminate\Http\Request;
 
 class ManageOrdersController extends Controller
 {
+
     public function index(){
-        $orders = Order::query()->get();
-        return view('management.employee.orders.index', compact('orders'));
+        $groupedOrders = Order::query()->get()->groupBy('status');
+        return view('management.employee.orders.index', compact('groupedOrders'));
     }
     public function show(Order $order){
         return view('management.employee.orders.show', compact('order'));
--- resources/views/management/employee/orders/index.blade.php
+++ resources/views/management/employee/orders/index.blade.php
@@ -1,26 +1,27 @@
 @include('shared.return-message')
-
-<table>
-    <tr>
-        <th>ID</th>
-        <th>name</th>
-        <th>Status</th>
-        <th>created at</th>
-        <th>Details</th>
-    </tr>
-    @foreach($orders as $order)
+@foreach($groupedOrders as $status => $orders)
+    {{ $status }} orders
+    <table>
         <tr>
-            <td>{{ $order->id }}</td>
-            <td>{{ $order->user->name }}</td>
-            <td>{{ $order->status }}</td>
-            <td>{{ $order->created_at }}</td>
-            <td>
-                <a href="{{route('management.employee.orders.show', $order)}}">Show details</a>
-            </td>
+            <th>ID</th>
+            <th>Client</th>
+            <th>Status</th>
+            <th>created at</th>
+            <th>Details</th>
         </tr>
-    @endforeach
-</table>
-
+        @foreach($orders as $order)
+            <tr>
+                <td>{{ $order->id }}</td>
+                <td>{{ $order->user->name }}</td>
+                <td>{{ $order->status }}</td>
+                <td>{{ $order->created_at }}</td>
+                <td>
+                    <a href="{{route('management.employee.orders.show', $order)}}">Show details</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endforeach
 <style>
     table, tr, td, th{
         border:1px solid black;
