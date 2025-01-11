6bd01c20cdd97a9885f9d11d5f22f027427d55fc
KacperW <kacek158@gmail.com>
2025-01-11 02:27:11 +0100

    Code formatting in EmployeesController.php

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -8,18 +8,26 @@ use Illuminate\Http\Request;
 
 class EmployeesController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $users = User::get();
+
         return view('management.admin.employees.index', compact('users'));
     }
 
-    public function destroy($id, LogDismissalEmployeeAction $logDismissalEmployeeAction){
+    public function destroy($id, LogDismissalEmployeeAction $logDismissalEmployeeAction)
+    {
         $user = User::find($id);
-        if(!$user)
-            return redirect()->route('management.admin.employees.index')->with('error', 'Nie udało się usunąć użytkownika');
+
+        if (!$user) {
+            return redirect()->route('management.admin.employees.index')->with('error',
+                'Nie udało się usunąć użytkownika');
+        }
 
         $logDismissalEmployeeAction->execute(auth()->id(), ['email' => $user->email]);
+
         $user->delete();
+
         return redirect()->route('management.admin.employees.index')->with('success', 'Użytkownik został usunięty');
     }
 }
