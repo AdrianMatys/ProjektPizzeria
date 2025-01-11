71ea37de12f9a75b1801b998229397dda70775df
KacperW <kacek158@gmail.com>
2025-01-11 02:25:19 +0100

    Code formatting

--- app/Http/Controllers/ClientOrdersController.php
+++ app/Http/Controllers/ClientOrdersController.php
@@ -9,21 +9,27 @@ use Illuminate\Http\Request;
 
 class ClientOrdersController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $userId = $request->user()->id;
         $groupedOrders = Order::query()
-            ->where('user_id', $userId )
+            ->where('user_id', $userId)
             ->whereIn('status', ['pending', 'in_progress'])
             ->get()
             ->groupBy('status');
         return view('client.orders.index', compact('groupedOrders'));
     }
-    public function show(Order $order){
+
+    public function show(Order $order)
+    {
         return view('client.orders.show', compact('order'));
     }
-    public function cancelOrder(CancelOrderRequest $request, Order $order){
-        if($order->status != 'pending')
+
+    public function cancelOrder(CancelOrderRequest $request, Order $order)
+    {
+        if ($order->status != 'pending') {
             return redirect()->back()->with('error', 'Możesz anulować tylko zamówienia posiadają status pending');
+        }
 
         $order->update(['status' => $request->validated()['status']]);
         return redirect(route('client.orders.index'))->with('success', 'Twoje zamówienie zostało anulowane');
