417915f471f7f37a0e457629bd7d63561d566ccf
KacperW <kacek158@gmail.com>
2025-01-19 01:00:25 +0100

    Add translatedName attribute in ingredient model

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\App;
 
 class Ingredient extends Model
 {
@@ -19,5 +21,19 @@ class Ingredient extends Model
     {
         return $this->belongsToMany(Pizza::class, 'pizza_ingredients', 'ingredient_id', 'pizza_id')
             ->withPivot('quantity');
+    }
+
+    public function translatedName(): Attribute
+    {
+        $locale = App::getLocale();
+        $name = $this->name;
+        $translation = $this->translations()->where('language_code', $locale)
+            ->first();
+        if($translation){
+            $name = $translation->name;
+        }
+        return Attribute::get(
+            fn(): string => $name,
+        );
     }
 }
