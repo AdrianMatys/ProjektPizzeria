8e1835fc11abee7d24f7884b323750aad251105b
KacperW <kacek158@gmail.com>
2025-01-20 21:05:06 +0100

    Add totalPrice in order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,12 @@ class Order extends Model
     }
     public function orderItems(){
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function totalPrice(): Attribute
+    {
+        return Attribute::get(
+            fn(): float => $this->orderItems->sum('price'),
+        );
     }
 }
