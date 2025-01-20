23ff4973c78c610bebe308cc07315ea096b19595
KacperW <kacek158@gmail.com>
2025-01-20 20:56:07 +0100

    Add totalPrice to cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -29,5 +30,12 @@ class Cart extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function totalPrice(): Attribute
+    {
+        return Attribute::get(
+            fn(): float => $this->items->sum('price'),
+        );
     }
 }
